78cb3842d6852f5483679d7f4c2d84507f5bd876
bprigent <hello@bprigent.com>
2024-11-28 08:27:07 -0500

    minor timer bugs

--- react_directory/src/components/ButtonGroup.js
+++ react_directory/src/components/ButtonGroup.js
@@ -4,27 +4,31 @@ import { ToggleButton, ToggleButtonGroup, Typography, Box } from '@mui/material'
 
 const ButtonGroup = ({ label, options, value, onChange }) => {
 
-    const [selectedValue, setSelectedValue] = useState(value || ''); // Start with provided value or empty
+    const [selectedValue, setSelectedValue] = useState(value); // Start with provided value
 
     useEffect(() => {
-        // Automatically set default option if no value is provided
-        if (!value) {
-          const defaultOption = options.find((option) => option.defaultValue);
-          if (defaultOption) {
-            setSelectedValue(defaultOption.value);
-            onChange(defaultOption.value); // Notify parent about the default selection
-          }
+        // Update the internal state when the value prop changes
+        if (value) {
+            setSelectedValue(value);
+        } else {
+            // If no value is provided, set the default value
+            const defaultOption = options.find((option) => option.defaultValue);
+            if (defaultOption && defaultOption.value !== selectedValue) {
+                setSelectedValue(defaultOption.value);
+                if (onChange) onChange(null, defaultOption.value); // Notify parent of default value
+            }
         }
-      }, [value, options, onChange]);
+    }, [value, options, selectedValue, onChange]);
 
+    // Prevent deselecting (newValue is null when clicking the selected button)
     const handleChange = (_, newValue) => {
-        // Prevent deselecting (newValue is null when clicking the selected button)
         if (newValue !== null) {
             setSelectedValue(newValue);
-            onChange(newValue); // Pass the new value to the parent
+            if (onChange) onChange(null, newValue); // Notify parent of the new selection
         }
     };
 
+    // return component
     return (
         <Box mb={3}>
             <Typography 
@@ -35,7 +39,7 @@ const ButtonGroup = ({ label, options, value, onChange }) => {
                 fontWeight: 'regular', 
                 }}
             >
-            {label}
+                {label}
             </Typography>
             <ToggleButtonGroup
                 value={selectedValue}
@@ -43,36 +47,36 @@ const ButtonGroup = ({ label, options, value, onChange }) => {
                 onChange={handleChange}
                 sx={{ display: 'flex', gap: 0 }}
             >
-            {options.map((option) => (
-                <ToggleButton
-                key={option.value}
-                value={option.value}
-                sx={{
-                    borderRadius: '6px',
-                    border: '1px solid #E5E5E5',
-                    textTransform: 'none',
-                    padding: '8px 16px',
-                    color: '#787878',
-                    '&:hover': {
-                    backgroundColor: '#ffffff', 
-                    color: '#000000', 
-                    border: '1px solid #E5E5E5',
-                    },
-                    '&.Mui-selected': {
-                        backgroundColor: '#E5EEFF',
-                        color: '#1976d2', 
-                        border: '1px solid #E5E5E5',
-                        '&:hover': {
-                            backgroundColor: '#E5EEFF', 
-                            color: '#1976d2', 
+                {options.map((option) => (
+                    <ToggleButton
+                        key={option.value}
+                        value={option.value}
+                        sx={{
+                            borderRadius: '6px',
                             border: '1px solid #E5E5E5',
-                        },
-                    },
-                }}
-                >
-                {option.label}
-                </ToggleButton>
-            ))}
+                            textTransform: 'none',
+                            padding: '8px 16px',
+                            color: '#787878',
+                            '&:hover': {
+                            backgroundColor: '#ffffff', 
+                            color: '#000000', 
+                            border: '1px solid #E5E5E5',
+                            },
+                            '&.Mui-selected': {
+                                backgroundColor: '#E5EEFF',
+                                color: '#1976d2', 
+                                border: '1px solid #E5E5E5',
+                                '&:hover': {
+                                    backgroundColor: '#E5EEFF', 
+                                    color: '#1976d2', 
+                                    border: '1px solid #E5E5E5',
+                                },
+                            },
+                        }}
+                    >
+                        {option.label}
+                    </ToggleButton>
+                ))}
             </ToggleButtonGroup>
         </Box>
     )
