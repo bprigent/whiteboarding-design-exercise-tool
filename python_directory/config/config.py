251b752f256b2b222a633a2b48653639d048f82c
bprigent <hello@bprigent.com>
2024-12-10 12:59:27 -0500

    minor edits

--- python_directory/config/config.py
+++ python_directory/config/config.py
@@ -2,7 +2,5 @@ import os
 
 # Define the path to your model directory
 MODEL_PATH = os.getenv("MODEL_PATH", "/Users/prige/Desktop/llama_project/python_directory/llm_model")
-NEW_MODEL_PATH = os.getenv("MODEL_PATH", "/Users/prige/Desktop/llama_project/python_directory/llm_model_2")
-
 
 FINE_TUNED_MODEL_PATH = os.path.join("models", "fine_tuned_models")
